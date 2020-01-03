89a1ca7e0cfe1d08a15e3226904613e93f861485
Hanjeong Lee <gkswjdzz@naver.com>
2020-01-03 16:39:48 +0900

    merge post url

--- server.js
+++ server.js
@@ -11,65 +11,59 @@ var output = path.join(__dirname, "output.jpg");
 var fullUrl = "";
 
 app.get("/", function(req, res) {
-  fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
-  console.log(fullUrl);
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(
-    '<form action="' +
-      fullUrl +
-      'fileupload" method="post" enctype="multipart/form-data">'
-  );
-  res.write('<input type="file" name="filetoupload"><br>');
-  res.write('<input type="submit">');
-  res.write("</form>");
-  return res.end();
+    fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
+    console.log(fullUrl);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(
+    '<form action="/" method="post" enctype="multipart/form-data">'
+    );
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write("</form>");
+    return res.end();
 });
 
-app.post("/readfile", async (req, res) => {
-  console.log(input, output);
-  const { i, o } = await runPython(input, output);
-  console.log(i, o);
-  console.log("start readfile");
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<html><body>");
-
-  console.log(output);
-
-  fs.readFile(output, (err, data) => {
-    if (err) throw err;
-    res.write('<img src="data:image/jpeg;base64,');
-    res.write(Buffer.from(data).toString("base64"));
-    res.write('"/>');
-    res.end("</body></html>");
-  });
-  console.log("end readfile");
-});
-
-app.post("/fileupload", function(req, res) {
-  var fileuploaded = true;
-  var busboy = new Busboy({ headers: req.headers });
-  busboy.on("file", function(fieldname, file, filename, encoding, mimetype) {
+app.post("/", function(req, res) {
+    var fileuploaded = true;
+    var busboy = new Busboy({ headers: req.headers });
+    busboy.on("file", function(fieldname, file, filename, encoding, mimetype) {
     if(filename === ""){
-      fileuploaded = false;
-      console.log("here");
+        fileuploaded = false;
+        console.log("here");
     }
     console.log(fieldname, filename);
     file.pipe(fs.createWriteStream(input));
-  });
+    });
 
-  busboy.on("finish", function() {
-    console.log("Upload complete");
-    if(!fileuploaded) {
-      res.writeHead(400);
-      res.end();
-      return;
-    }
+    busboy.on("finish", async function() {
+        console.log("Upload complete");
+        if(!fileuploaded) {
+            res.writeHead(400);
+            res.end();
+            return;
+        }
 
-    res.redirect(307, fullUrl + "readfile");
-  });
+        console.log(input, output);
+        const { i, o } = await runPython(input, output);
+        console.log(i, o);
+        console.log("start readfile");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<html><body>");
 
-  req.pipe(busboy);
-  console.log("end fileupload post");
+        console.log(output);
+
+        fs.readFile(output, (err, data) => {
+            if (err) throw err;
+            res.write('<img src="data:image/jpeg;base64,');
+            res.write(Buffer.from(data).toString("base64"));
+            res.write('"/>');
+            res.end("</body></html>");
+        });
+        console.log("end readfile");
+    });
+
+    req.pipe(busboy);
+    console.log("end fileupload post");
 });
 
 app.listen(80, () => {
@@ -77,23 +71,23 @@ app.listen(80, () => {
 });
 
 runPython = (input, output) => {
-  return new Promise((resolve, reject) => {
-    PythonShell.run(
-      __dirname + "/detectron2_repo/demo.py",
-      { args: [input, output] },
-      async (err, result) => {
-        if (err) {
-          if (err.traceback === undefined) {
-            console.log(err.message);
-          } else {
-            console.log(err.traceback);
-          }
-        }
-        const inputdir = await result[result.length - 2];
-        const outputdir = await result[result.length - 1];
-        console.log(inputdir, outputdir);
-        resolve({ inputdir, outputdir });
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        PythonShell.run(
+            __dirname + "/detectron2_repo/demo.py",
+            { args: [input, output] },
+            async (err, result) => {
+                if (err) {
+                    if (err.traceback === undefined) {
+                        console.log(err.message);
+                    } else {
+                        console.log(err.traceback);
+                    }
+                }
+                const inputdir = await result[result.length - 2];
+                const outputdir = await result[result.length - 1];
+                console.log(inputdir, outputdir);
+                resolve({ inputdir, outputdir });
+            }
+        );
+    });
 };
